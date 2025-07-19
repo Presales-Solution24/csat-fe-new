6932d87250ed38c09b302bc04d9c4665fb3c8cc6
RadityaIP <66769805+radityaip@users.noreply.github.com>
2025-07-19 09:44:31 +0700

    update to BE production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // Ganti jika backend Anda berbeda
+  baseURL: "https://uat-be.solution-core.com", // Ganti jika backend Anda berbeda
   headers: {
     "Content-Type": "application/json",
   },
